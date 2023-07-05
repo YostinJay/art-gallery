12ae3d56738997fe1bf36ecd3b0747037a2110f9
YostinJay <justin_jay_cabonce@yahoo.com>
2023-07-05 11:31:05 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,1 @@
 const swup = new Swup();
-
-document.addEventListener("click", function handleClick(event) {
-  event.target.classList.add("new-active");
-
-  event.target.classList.remove("new-active");
-});
